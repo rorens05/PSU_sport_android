2752998f14b9d7ccc8189149e34e62ac8edd70a2
Laurence Bautista <rorens05@gmail.com>
2019-02-25 16:14:28 +0800

    events retrieving and displaying

--- app/src/main/java/com/example/psusports/EventsActivity.java
+++ app/src/main/java/com/example/psusports/EventsActivity.java
@@ -1,5 +1,6 @@
 package com.example.psusports;
 
+import android.app.ProgressDialog;
 import android.content.DialogInterface;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
@@ -8,17 +9,31 @@ import android.support.v7.widget.DividerItemDecoration;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
+import android.widget.Toast;
 
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.example.psusports.engine.MySingleton;
+import com.example.psusports.global.GlobalVariables;
 import com.example.psusports.models.SportEvent;
 import com.example.psusports.templates.SportEventAdapter;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
 public class EventsActivity extends AppCompatActivity {
     private static final String TAG = "EventsActivity";
     RecyclerView recyclerView;
     SportEventAdapter sportEventAdapter;
+    ProgressDialog pd;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -62,40 +77,91 @@ public class EventsActivity extends AppCompatActivity {
     }
 
     public void loadEvents(){
-        List<SportEvent> eventList = new ArrayList<>();
 
-        Log.d(TAG, "Loading sample data");
         //<editor-fold desc="sample data">
-        SportEvent sb = new SportEvent();
-        sb.name = "event1";
-        sb.event_date = "date1";
-        sb.organizer = "organizer1";
-        sb.venue = "venue1";
-        eventList.add(sb);
-
-        sb = new SportEvent();
-        sb.name = "event2";
-        sb.event_date = "date2";
-        sb.organizer = "organizer2";
-        sb.venue = "venue2";
-        eventList.add(sb);
-
-        sb = new SportEvent();
-        sb.name = "event1";
-        sb.event_date = "date1";
-        sb.organizer = "organizer1";
-        sb.venue = "venue1";
-        eventList.add(sb);
+//        List<SportEvent> eventList = new ArrayList<>();
+//        Log.d(TAG, "Loading sample data");
+//        SportEvent sb = new SportEvent();
+//        sb.name = "event1";
+//        sb.event_date = "date1";
+//        sb.organizer = "organizer1";
+//        sb.venue = "venue1";
+//        eventList.add(sb);
+//
+//        sb = new SportEvent();
+//        sb.name = "event2";
+//        sb.event_date = "date2";
+//        sb.organizer = "organizer2";
+//        sb.venue = "venue2";
+//        eventList.add(sb);
+//
+//        sb = new SportEvent();
+//        sb.name = "event1";
+//        sb.event_date = "date1";
+//        sb.organizer = "organizer1";
+//        sb.venue = "venue1";
+//        eventList.add(sb);
         //</editor-fold>
 
-        Log.d(TAG, "data loaded");
+        pd = new ProgressDialog(EventsActivity.this);
+        pd.setMessage("Loading Events");
+        pd.show();
 
-        Log.d(TAG, "initializing adapter");
-        sportEventAdapter = new SportEventAdapter(eventList, this);
+        StringRequest events = new StringRequest(Request.Method.GET, GlobalVariables.EVENT_URL, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                Log.d(TAG, "Response Recieved");
+                try {
+                    JSONObject jResponse = new JSONObject(response);
+                    JSONArray data = jResponse.getJSONArray("data");
+                    Log.d(TAG, "Looping through data, size: " + data.length());
+                    for (int i = 0; i < data.length(); i++) {
+                        JSONObject temp = data.getJSONObject(i);
+                        SportEvent event = new SportEvent();
+                        event.id = temp.getString("id");
+                        event.name = temp.getString("name");
+                        event.event_date = temp.getString("event_date");
+                        event.venue = temp.getString("venue");
+                        event.organizer = temp.getString("organizer");
+                        Log.d(TAG, event.toString());
+                        GlobalVariables.eventList.add(event);
+                    }
+                    Log.d(TAG, "Events Loaded Successfully");
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                    Log.d(TAG, "Response error, Loading failed");
+                }
+
+                loadRecyclerView();
+                pd.dismiss();
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Log.d(TAG, "Response Recieved");
+                Toast.makeText(EventsActivity.this, "Failed to connect to the server", Toast.LENGTH_SHORT).show();
+                pd.dismiss();
+            }
+        }){
+            @Override
+            protected Map<String, String> getParams() throws AuthFailureError {
+                return super.getParams();
+            }
+        };
+
+        MySingleton.getInstance(this).addToRequestQueue(events);
+
+        Log.d(TAG, "eventsloaded");
+
+    }
+
+    public void loadRecyclerView(){
+        Log.d(TAG, "initializing recyclerview adapter");
+        sportEventAdapter = new SportEventAdapter(GlobalVariables.eventList, this);
         recyclerView.setAdapter(sportEventAdapter);
         Log.d(TAG, "adapter initialized ");
 
-
+        Log.d(TAG, "Displaying Data");
         LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this);
         linearLayoutManager.setOrientation(LinearLayoutManager.VERTICAL);
         DividerItemDecoration dividerItemDecoration = new DividerItemDecoration(recyclerView.getContext(),
@@ -104,4 +170,5 @@ public class EventsActivity extends AppCompatActivity {
         recyclerView.setLayoutManager(linearLayoutManager);
 
     }
+
 }
--- app/src/main/java/com/example/psusports/global/GlobalVariables.java
+++ app/src/main/java/com/example/psusports/global/GlobalVariables.java
@@ -1,12 +1,16 @@
 package com.example.psusports.global;
 
+import com.example.psusports.models.SportEvent;
 import com.example.psusports.models.User;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class GlobalVariables {
     public static final String SERVER_URL = "http://192.168.43.194:3000/api/v1/";
     public static final String LOGIN_URL = SERVER_URL + "access/attempt_login";
+    public static final String EVENT_URL = SERVER_URL + "editor/events";
 
-
-
-    public static User currentUser;
+    public static User currentUser = new User();
+    public static List<SportEvent> eventList = new ArrayList<>();
 }
